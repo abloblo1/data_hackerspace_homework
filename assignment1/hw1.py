03150e119d2e19770d763df37cebbeb69850c9fc
Andrew Chao <ahchao3@siebl-0220-14.ews.illinois.edu>
2018-10-01 22:25:01 -0500

    done

--- assignment1/hw1.py
+++ assignment1/hw1.py
@@ -50,51 +50,53 @@ def single_type_candy_count(filename):
     totalCandies = 0
     jsonResponse = json.loads(open(filename).read())
     for item in jsonResponse["pokemon"]:
-        try:
-            amount = item["candy_count"]
-            totalCandies += amount
-        except KeyError:
-            totalCandies += 0
+        if len(item["type"]) == 1:
+            try:
+                amount = item["candy_count"]
+                totalCandies += amount
+            except KeyError:
+                totalCandies += 0
     return totalCandies
 
 
-
-
-
-
 def reflections_and_projections(points):
-    newPoints1 = np.flip(points, (0, 1))
+    flip = [[1,0],[0,-1]]
+    newPoints1 = np.matmul(flip,points)
+    for i in range(len(newPoints1[1])):
+        newPoints1[1][i] += 1
 
-    rotation = [[0,-1],[1,0]]
-    newPoints2 = np.matmul(rotation,newPoints1)
+    rotation = np.matrix([[math.cos(math.pi/2), -math.sin(math.pi/2)], [math.sin(math.pi/2), math.cos(math.pi/2)]])
+    newPoints2 = np.matmul(rotation, newPoints1)
 
-    project = [[0.1,0.3],[0.3,0.9]]
-    newPoints3 = np.matmul((project,newPoints2))
-
+    projection = np.matrix([[1, 3], [3, 3**2]])/(3**2 + 1)
+    newPoints3 = np.matmul(projection, newPoints2)
     return newPoints3
-
-
-
 
 
 
 def normalize(image):
     maxInt = np.amax(image)
     minInt = np.amin(image)
-    newList = ((255)/(maxInt-minInt) * (image - minInt))
+    newList = (255.0/(maxInt-minInt)) * (image - minInt)
     return newList
 
 
+def sigmoid_normalize(image, a):
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            image[i][j] = int(round(( 255.0 * (( (1 + (math.e**( -(1.0/a)*(image[i][j] - 128) )) ) )**-1))))
+    return image
 
 
 
-def sigmoid_normalize(image):
-    maxInt = np.amax(image)
-    alist = [i / maxInt for i in image]
-    alist = [1 / (1 + math.exp(-i)) for i in image]
-
-
-
+print("\n")
 print(weigh_pokemons("pokedex.json",10.0))
 print(single_type_candy_count("pokedex.json"))
 print(histogram_times("airplane_crashes.csv"))
+print(reflections_and_projections([[3,5],[8,2]]))
+image = np.array([[60, 70, 90, 100],
+                  [80, 90, 55, 120],
+                  [205, 122, 87, 90],
+                  [103, 145, 140, 104]])
+print("asdfasdf",normalize(image))
+print(sigmoid_normalize(image, 100))
