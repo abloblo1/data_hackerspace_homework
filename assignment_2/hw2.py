4b1c9d48bd4a99ff1ed975dfa8472eeb9ae82d57
abloblo1 <andrew739@gmail.com>
2018-10-03 21:04:47 -0500

    First committ

---
+++ assignment_2/hw2.py
@@ -0,0 +1,13 @@
+import requests
+import re
+import matplotlib.pyplot as plt
+
+
+def lyrics_word_count_easy(artist, song, phrase):
+    pass
+
+def lyrics_word_count(artist, phrase):
+    pass
+
+def visualize():
+    pasfwefwefwef
